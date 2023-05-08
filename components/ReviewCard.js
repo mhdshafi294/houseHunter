cc359c168745108bcd34f5f8b18452b130dfed9d
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-08 17:48:34 +0300

    implement module styling reviews section

--- components/ReviewCard.js
+++ components/ReviewCard.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import Image from 'next/image'
+import styles from '../styles/ReviewCard.module.css'
 
 export default function ReviewCard(props){
     return (
-        <div className='review-card'>
+        <div className={styles.reviewCard}>
             <Image src={props.image}  className='review-house-pic' width='740' height='400'/>
-            <div className='review-content'>
-                <h4 className='review-title'>{props.title}</h4>
-                <p className='review-details'>{props.description}</p>
-                <div className='review-person-rate'>
-                    <div className='review-person'>
-                        <Image src={props.userImage} className='review-person-pic' width='40' height='40'/>
-                        <div className='review-person-name-job'>
-                            <p className='review-person-name'>{props.userName}</p>
-                            <p className='review-person-job'>{props.userJob}</p>
+            <div className={styles.reviewContent}>
+                <h4 className={styles.reviewTitle}>{props.title}</h4>
+                <p className={styles.reviewDetails}>{props.description}</p>
+                <div className={styles.reviewPersonRate}>
+                    <div className={styles.reviewPerson}>
+                        <Image src={props.userImage} className={styles.reviewPersonPic} width='40' height='40'/>
+                        <div className={styles.reviewPersonNameJob}>
+                            <p className={styles.reviewPersonName}>{props.userName}</p>
+                            <p className={styles.reviewPersonJob}>{props.userJob}</p>
                         </div>
                     </div>
-                    <div className='review-rate'>
-                        <Image src={'/../public/imgs/vector.png'} className='review-star'width='23' height='23'/>
-                        <p className='review-rate'>{props.rate}</p>
+                    <div className={styles.reviewRate}>
+                        <Image src={'/../public/imgs/vector.png'} className={styles.reviewStar} width='23' height='23'/>
+                        <p className={styles.reviewRateText}>{props.rate}</p>
                     </div>
                 </div>
             </div>
         </div>
-      )
+    )
 }
