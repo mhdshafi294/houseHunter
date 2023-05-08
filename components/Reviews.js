f7f672dff10080689f246df3365ab29fe0c65c5a
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-08 13:56:13 +0300

    editting reviews section

--- components/Reviews.js
+++ components/Reviews.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import ReviewCard from './ReviewCard'
 import { reviews } from '../constants/constants.js'
+import { useState } from 'react';
 
 export default function Reviews(){
+    const [activeIndex, setActiveIndex] = useState(0);
+    const carouselRef = useRef();
+
+    const updateIndex = (newIndex) => {
+        if (newIndex < 0) {
+            newIndex = 0;
+        } else if (newIndex >= reviews.length) {
+            newIndex = reviews.length - 1;
+        }
+        setActiveIndex(newIndex);
+    };
+
     return (
         <div className='reviews'>
             <div className='divider'></div>
             <p className='section-title-small'>See Our Review</p>
             <h2 className='section-title-large'>What Our User Say About Us</h2>
-            <div className='ReviewCards'>
+            <div ref={carouselRef} className='ReviewCards'>
                 {
                 reviews.map((review, index) =>(
                     <ReviewCard 
@@ -23,6 +36,24 @@ export default function Reviews(){
                     />
                 ))}
             </div>
+            <div className="indicators">
+                {reviews.map((item, index) => {
+                    return (
+                    <button
+                        key={index}
+                        className={`indicator-buttons ${
+                            index === activeIndex
+                            ? "indicator-symbol-active"
+                            : "indicator-symbol"
+                        }`}
+                        onClick={() => {
+                        updateIndex(index);
+                        }}
+                    >
+                    </button>
+                    );
+                })}
+            </div>
         </div>
-      )
+        )
 }
