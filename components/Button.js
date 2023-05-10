8dc6c599159788582159c6006ff6a5c383c22238
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-10 16:33:32 +0300

    working on the navbar4

---
+++ components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/button.module.css'
+
+export default function Button(){
+
+    return(
+        <div className='button'>
+
+        </div>
+    )
+}
