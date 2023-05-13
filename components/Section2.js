1a9bc8d82b5a7b84e347c90302cc4151b8f668f4
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-13 22:53:11 +0300

    continue working on section2 2

--- components/Section2.js
+++ components/Section2.js
@@ -1,21 +1,28 @@
 
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Section2.module.css'
 
 export default function Section2(){
     return(
         <div className={styles.section2}>
-            <div className={styles.title}>
-                <div className={styles.smallTitle}>
-                    <div className={styles.divider}></div>
-                    <p className={styles.smallTitleText}>Our Recommendation</p>
+            <div className={styles.sectionHeader}>
+                <div className={styles.title}>
+                    <div className={styles.smallTitle}>
+                        <div className={styles.divider}></div>
+                        <p className={styles.smallTitleText}>Our Recommendation</p>
+                    </div>
+                    <h2 className={styles.largeTitle}>Featured House</h2>
                 </div>
-                <h2 className={styles.largeTitle}>Featured House</h2>
-            </div>
-            <div className={styles.navs}>
-                <Link  href='/about'><li>House</li></Link>
-                <Link  href='/article'><li>Villa</li></Link>
-                <Link  href='/property'><li>Apartment</li></Link>
+                <div className={styles.navs}>
+                    <Link  href='/about'><li className={styles.avtiveNav}>House</li></Link>
+                    <Link  href='/article'><li>Villa</li></Link>
+                    <Link  href='/property'><li>Apartment</li></Link>
+                </div>
+                <div className={styles.buttons}>
+                    <div className={styles.arrowLeft}><Image src="/../public/imgs/s2/eva_arrow-backward.png" width="28" height="28"/></div>
+                    <div className={styles.arrowRight}><Image src="/../public/imgs/s2/eva_arrow-forward.png" width="28" height="28"/></div>
+                </div>
             </div>
         </div>
     )
