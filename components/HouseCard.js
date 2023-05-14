d4f608109a8eadc8da613e3484997fcb8adfb6a3
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-14 21:42:49 +0300

    working on houseCard on section2 2

--- components/HouseCard.js
+++ components/HouseCard.js
@@ -4,7 +4,17 @@ import styles from '../styles/HouseCard.module.css'
 export default function HouseCard({house}){
     return(
         <div className={styles.houseCard}>
-            <div className={styles.price}>{house.price}</div>
+            <div className={styles.badge}>{house.badge}</div>
+            <Image src={house.image} className={styles.houseImage} width='340' height='382'/>
+            <p className={styles.name}>{house.name}</p>
+            <p className={styles.price}>$ {house.price}</p>
+            <div className={styles.owner}>
+                <Image src={house.ownerImage} className={styles.ownerImage} width='40' height='40'/>
+                <div className={styles.nameAndLocation}>
+                    <p className={styles.ownerName}>{house.ownerName}</p>
+                    <p className={styles.location}>{house.location}</p>
+                </div>
+            </div>
         </div>
     )
 }
