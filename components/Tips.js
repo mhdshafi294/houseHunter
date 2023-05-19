1eab8d6ad11a030a911c907aefffab4f45c55914
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-19 19:24:53 +0300

    working on section Tips largeTip

--- components/Tips.js
+++ components/Tips.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from '../styles/Tips.module.css'
 import Tip from './Tip'
 import { tipsInfo } from '../constants/constants.js'
+import { AiFillClockCircle } from 'react-icons/ai';
 
 export default function Tips(){
     return(
@@ -19,7 +20,25 @@ export default function Tips(){
                         />
                     ))}
                 </div>
-                <div className={styles.largeTip}></div>
+                <div className={styles.largeTip}>
+                    <div className={styles.tip}>
+                        <div className={styles.imgDiv}>
+                            <Image className={styles.img} src={tipsInfo[tipsInfo.length - 1].image} width="200" height="144"/>
+                        </div>
+                        <div className={styles.right}>
+                            <div className={styles.author}>
+                                <Image className={styles.authorImg} src={tipsInfo[tipsInfo.length - 1].authorImage} width="32" height="32"/>
+                                <p className={styles.authorName}>{tipsInfo[tipsInfo.length - 1].authorName}</p>
+                            </div>
+                            <h5 className={styles.title}>{tipsInfo[tipsInfo.length - 1].title}</h5>
+                            <p className={styles.description}>{tipsInfo[tipsInfo.length - 1].title}</p>
+                            <div className={styles.durationAndDate}>
+                                <p className={styles.clock}> <AiFillClockCircle /></p>
+                                <p>{`${tipsInfo[tipsInfo.length - 1].readDuration} min read |  ${tipsInfo[tipsInfo.length - 1].publishDate}`}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
